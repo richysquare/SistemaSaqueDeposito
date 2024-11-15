c708f0429f71b5417b96e74e522cd0dbc821e5a6
Ricardo Toichoa <ricardotoichoa07@gmail.com>
2024-11-15 09:01:55 -0300

    Criadas classes Account e BusinessAccount

---
+++ SaqueDeposito/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaqueDeposito.Entities
+{
+    public class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ SaqueDeposito/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaqueDeposito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
